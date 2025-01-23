5601d90dae333146d3230ed7f50a7c6155092dec
Dan Lund <dan.lundh@outlook.com>
2025-01-23 10:12:11 +0100

    fixed formating

--- draw_graph.py
+++ draw_graph.py
@@ -1,16 +1,9 @@
-import pandas as pd 
-import numpy as np 
-import matplotlib.pyplot as plt 
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
 
-
-
-
-
-
-
-
-df = pd.DataFrame(  {"x": np.array([1,2,3]), "y": np.array([2,5 , 8])}    )
+df = pd.DataFrame({"x": np.array([1, 2, 3]), "y": np.array([2, 5, 8])})
 
 print(df)
 
